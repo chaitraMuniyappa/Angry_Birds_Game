739f7ec510ff8bdf9998fff91ffe524012a8e85d
Chaitra Muniyappa <chaitra.muniyappa@gmail.com>
2021-03-05 12:27:17 +0530

    class27

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -59,6 +62,8 @@ function draw(){
     log4.display();
     log5.display();
 
+    log6.display();
+    chain.display();
     bird.display();
     platform.display();
 }
